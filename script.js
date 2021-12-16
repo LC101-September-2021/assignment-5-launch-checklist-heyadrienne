3b9d334fdc0f7cd86f87175aa3b4ac6fbef7fd7e
Adrienne Meltzer <adriennemeltzer@gmail.com>
2021-12-16 11:38:13 -0500

    Commenting out Script.js Line 3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Write your JavaScript code here!
 
-const { addDestinationInfo, formSubmission } = require("./scriptHelper");
+//const { addDestinationInfo, formSubmission } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
 
